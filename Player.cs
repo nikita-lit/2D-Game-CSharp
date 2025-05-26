84f17e1776cfff7b3085d570d9580b5863ccadf8
Opilane TTHK <nikitalitvinenko28@gmail.com>
2025-05-26 15:28:39 +0300

    init

---
+++ Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Numerics;
+
+namespace Game2D
+{
+    public class Player
+    {
+        public Vector2 Position;
+
+        public void Update()
+        {
+            if (Raylib.IsKeyDown(KeyboardKey.W))
+            {
+                Position.Y -= 0.01f;
+            }
+            else if(Raylib.IsKeyDown(KeyboardKey.S))
+            {
+                Position.Y += 0.01f;
+            }
+
+            if (Raylib.IsKeyDown(KeyboardKey.D))
+            {
+                Position.X += 0.01f;
+            }
+            else if (Raylib.IsKeyDown(KeyboardKey.A))
+            {
+                Position.X -= 0.01f;
+            }
+        }
+
+        public void Draw()
+        {
+            Raylib.DrawRectangle((int)Position.X, (int)Position.Y, 25, 25, Color.Brown);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿global using Raylib_cs;
+using System.Numerics;
+
+namespace Game2D
+{
+    class Program
+    {
+        public static Player Player;
+
+        public static void Main()
+        {
+            Player = new Player();
+            Raylib.InitWindow(800, 480, "Hello World");
+            Run();
+        }
+
+        public static void Run()
+        {
+            while (!Raylib.WindowShouldClose())
+            {
+                Update();
+                Render();
+            }
+
+            Stop();
+        }
+
+        public static void Update()
+        {
+            Player.Update();
+        }
+
+        public static void Render()
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Black);
+
+            Player.Draw();
+
+            //Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+            //Raylib.DrawRectangle(0, 0, 50, 50, Color.Black);
+
+            Raylib.EndDrawing();
+        }
+
+        public static void Stop()
+        {
+            Raylib.CloseWindow();
+        }
+    }
+}
